95511af3428625d35a5c9db9f049610a25fa8f4d
GLidia <74815962+GLidia@users.noreply.github.com>
2022-06-02 16:09:46 +0300

    Add lesson 8 lab and problem descriptions

---
+++ 08_Generics/Lab/01_BoxOfT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+        private int count;
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public Box()
+        {
+            this.count = 0;
+            this.items = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.items.Add(element);
+            this.count++;
+        }
+
+        public T Remove()
+        {
+            var itemToReturn = this.items[this.count - 1];
+            this.items.RemoveAt(this.count - 1);
+            count--;
+            return itemToReturn;
+        }
+    }
+}
