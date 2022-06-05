ddde3cb4c995fa06f6eb8b5efe5db72e39a7973f
GLidia <74815962+GLidia@users.noreply.github.com>
2022-06-05 17:01:04 +0300

    Add practice exam 1

---
+++ Exams/01_Practice Exam From June 28, 2020/Bombs/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> bombEffects = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+            int daturaCount = 0;
+            int cherryCount = 0;
+            int smokeDecoyCount = 0;
+            bool isFull = false;
+
+            while (bombEffects.Count > 0 && bombCasings.Count > 0)
+            {
+                int currentEffect = bombEffects.Peek();
+                int currentCasing = bombCasings.Peek();
+
+                if (currentEffect + currentCasing == 40)
+                {
+                    daturaCount++;
+                    bombEffects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else if (currentEffect + currentCasing == 60)
+                {
+                    cherryCount++;
+                    bombEffects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else if (currentEffect + currentCasing == 120)
+                {
+                    smokeDecoyCount++;
+                    bombEffects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else
+                {
+                    bombCasings.Pop();
+                    bombCasings.Push(currentCasing - 5);
+                }
+
+                if (daturaCount >= 3 && cherryCount >= 3 && smokeDecoyCount >= 3)
+                {
+                    isFull = true;
+                    break;
+                }
+            }
+
+            if (isFull)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+            if (bombEffects.Count == 0)
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffects)}");
+            }
+
+            if (bombCasings.Count == 0)
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasings)}");
+            }
+
+            Console.WriteLine($"Cherry Bombs: {cherryCount}");
+            Console.WriteLine($"Datura Bombs: {daturaCount}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyCount}");
+        }
+    }
+}
---
+++ Exams/01_Practice Exam From June 28, 2020/Snake/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            int snakeRow = 0;
+            int snakeCol = 0;
+
+            bool foundFirstBurrow = false;
+
+            int firstBurrowRow = -1;
+            int firstBurrowCol = -1;
+            int secBurrowRow = -1;
+            int secBurrowCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowInput = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                    if (rowInput[col] == 'S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                    else if(rowInput[col] == 'B')
+                    {
+                        if (!foundFirstBurrow)
+                        {
+                            firstBurrowRow = row;
+                            firstBurrowCol = col;
+                            foundFirstBurrow = true;
+                        }
+                        else
+                        {
+                            secBurrowRow = row;
+                            secBurrowCol = col;
+                        }
+                    }
+                }
+            }
+
+            int foodEaten = 0;
+            bool isOutside = false;
+            bool enoughFoodEaten = false;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "down")
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+                    if (snakeRow + 1 == n)
+                    {
+                        isOutside = true;
+                        break;
+                    }
+
+                    if (matrix[snakeRow + 1, snakeCol] == '-')
+                    {
+                        matrix[snakeRow + 1, snakeCol] = 'S';
+                        snakeRow++;
+                    }
+                    else if (matrix[snakeRow + 1, snakeCol] == '*')
+                    {
+                        foodEaten++;
+                        matrix[snakeRow + 1, snakeCol] = 'S';
+                        snakeRow++;
+                        if (foodEaten >= 10)
+                        {
+                            enoughFoodEaten = true;
+                            break;
+                        }
+                    }
+                    else if (matrix[snakeRow + 1, snakeCol] == 'B')
+                    {
+                        matrix[snakeRow + 1, snakeCol] = '.';
+                        if (snakeRow + 1 == firstBurrowRow && snakeCol == firstBurrowCol)
+                        {
+                            matrix[secBurrowRow, secBurrowCol] = 'S';
+                            snakeRow = secBurrowRow;
+                            snakeCol = secBurrowCol;
+                        }
+                        else
+                        {
+                            matrix[firstBurrowRow, firstBurrowCol] = 'S';
+                            snakeRow = firstBurrowRow;
+                            snakeCol = firstBurrowCol;
+                        }
+                    }
+                }
+                else if (command == "up")
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+                    if (snakeRow - 1 == -1)
+                    {
+                        isOutside = true;
+                        break;
+                    }
+
+                    if (matrix[snakeRow - 1, snakeCol] == '-')
+                    {
+                        matrix[snakeRow - 1, snakeCol] = 'S';
+                        snakeRow--;
+                    }
+                    else if (matrix[snakeRow - 1, snakeCol] == '*')
+                    {
+                        foodEaten++;
+                        matrix[snakeRow - 1, snakeCol] = 'S';
+                        snakeRow--;
+                        if (foodEaten >= 10)
+                        {
+                            enoughFoodEaten = true;
+                            break;
+                        }
+                    }
+                    else if (matrix[snakeRow - 1, snakeCol] == 'B')
+                    {
+                        matrix[snakeRow - 1, snakeCol] = '.';
+                        if (snakeRow - 1 == firstBurrowRow && snakeCol == firstBurrowCol)
+                        {
+                            matrix[secBurrowRow, secBurrowCol] = 'S';
+                            snakeRow = secBurrowRow;
+                            snakeCol = secBurrowCol;
+                        }
+                        else
+                        {
+                            matrix[firstBurrowRow, firstBurrowCol] = 'S';
+                            snakeRow = firstBurrowRow;
+                            snakeCol = firstBurrowCol;
+                        }
+                    }
+                }
+                else if (command == "left")
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+                    if (snakeCol - 1 == -1)
+                    {
+                        isOutside = true;
+                        break;
+                    }
+
+                    if (matrix[snakeRow, snakeCol - 1] == '-')
+                    {
+                        matrix[snakeRow, snakeCol - 1] = 'S';
+                        snakeCol--;
+                    }
+                    else if (matrix[snakeRow, snakeCol - 1] == '*')
+                    {
+                        foodEaten++;
+                        matrix[snakeRow, snakeCol - 1] = 'S';
+                        snakeCol--;
+                        if (foodEaten >= 10)
+                        {
+                            enoughFoodEaten = true;
+                            break;
+                        }
+                    }
+                    else if (matrix[snakeRow, snakeCol - 1] == 'B')
+                    {
+                        matrix[snakeRow, snakeCol - 1] = '.';
+                        if (snakeRow == firstBurrowRow && snakeCol - 1 == firstBurrowCol)
+                        {
+                            matrix[secBurrowRow, secBurrowCol] = 'S';
+                            snakeRow = secBurrowRow;
+                            snakeCol = secBurrowCol;
+                        }
+                        else
+                        {
+                            matrix[firstBurrowRow, firstBurrowCol] = 'S';
+                            snakeRow = firstBurrowRow;
+                            snakeCol = firstBurrowCol;
+                        }
+                    }
+                }
+                else if (command == "right")
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+                    if (snakeCol + 1 == n)
+                    {
+                        isOutside = true;
+                        break;
+                    }
+
+                    if (matrix[snakeRow, snakeCol + 1] == '-')
+                    {
+                        matrix[snakeRow, snakeCol + 1] = 'S';
+                        snakeCol++;
+                    }
+                    else if (matrix[snakeRow, snakeCol + 1] == '*')
+                    {
+                        foodEaten++;
+                        matrix[snakeRow, snakeCol + 1] = 'S';
+                        snakeCol++;
+                        if (foodEaten >= 10)
+                        {
+                            enoughFoodEaten = true;
+                            break;
+                        }
+                    }
+                    else if (matrix[snakeRow, snakeCol + 1] == 'B')
+                    {
+                        matrix[snakeRow, snakeCol + 1] = '.';
+                        if (snakeRow == firstBurrowRow && snakeCol + 1 == firstBurrowCol)
+                        {
+                            matrix[secBurrowRow, secBurrowCol] = 'S';
+                            snakeRow = secBurrowRow;
+                            snakeCol = secBurrowCol;
+                        }
+                        else
+                        {
+                            matrix[firstBurrowRow, firstBurrowCol] = 'S';
+                            snakeRow = firstBurrowRow;
+                            snakeCol = firstBurrowCol;
+                        }
+                    }
+                }
+
+
+                //for (int row = 0; row < n; row++)
+                //{
+                //    for (int col = 0; col < n; col++)
+                //    {
+                //        Console.Write(matrix[row, col]);
+                //    }
+                //    Console.WriteLine();
+                //}
+
+            }
+
+            if (isOutside)
+            {
+                Console.WriteLine("Game over!");
+            }
+            else
+            {
+                Console.WriteLine("You won! You fed the snake.");
+            }
+
+            Console.WriteLine($"Food eaten: {foodEaten}");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
